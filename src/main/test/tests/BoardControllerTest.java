0c9653c1f80965700cfe0772743372b39ea93034
TomashGombosh1 <tomash.gombosh@qatestlab.ue>
2019-09-22 19:35:01 +0300

    Add Unit test to the travis.yml

---
+++ src/main/test/tests/BoardControllerTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import controllers.trello.BoardController;
+import dataModels.BoardDataModel;
+import dataModels.MemberDataModel;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class BoardControllerTest {
+
+    @Test
+    @Parameters(value = {"boardName", "memberName", "boardId"})
+    public void getBoardId(String boardName, String memberName, String boardId){
+        MemberDataModel memberDataModel = new MemberDataModel();
+        memberDataModel.setId(memberName);
+        BoardDataModel boardDataModel = new BoardDataModel();
+        boardDataModel.setName(boardName);
+        String actualBoardId = new BoardController().getBoardId(memberDataModel, boardDataModel);
+        Assert.assertEquals(actualBoardId, boardId, "Board id should be the same");
+    }
+}
